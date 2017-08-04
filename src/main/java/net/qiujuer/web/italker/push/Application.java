96431ee4643fd6b87247cfb65e23c3d4055c0651
xxxidream <1657140647@qq.com>
2017-08-04 17:30:28 +0800

    web

---
+++ src/main/java/net/qiujuer/web/italker/push/Application.java
@@ -0,0 +1,20 @@
+package net.qiujuer.web.italker.push;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import java.util.logging.Logger;
+
+import net.qiujuer.web.italker.push.service.AccountService;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class Application extends ResourceConfig{
+    public Application(){
+        //注册逻辑处理的包名
+//        packages("net.qiujuer.web.italker.push.service.service");
+        packages(AccountService.class.getPackage().getName());
+
+        //注册Json解析器
+        register(JacksonJsonProvider.class);
+        //注册日志打印输出
+        register(Logger.class);
+    }
+}
---
+++ src/main/java/net/qiujuer/web/italker/push/service/AccountService.java
@@ -0,0 +1,28 @@
+package net.qiujuer.web.italker.push.service;
+
+import net.qiujuer.web.italker.push.bean.User;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
+
+@Path("/account")
+public class AccountService {
+    //GET 127.0.0.1/api/account/login
+    @GET
+    @Path("/login")
+    public String get() {
+        return "You get the login.";
+    }
+    @POST
+    @Path("/login")
+    //指定请求与返回的响应体为JSON
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public User post(){
+        User user = new User();
+        user.setName("美女");
+        user.setSex(2);
+        return user;
+    }
+}
