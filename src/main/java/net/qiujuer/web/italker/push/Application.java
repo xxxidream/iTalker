7ea7d101c14094dea64538ec147e542f26a9c4b7
xxxidream <1657140647@qq.com>
2017-08-16 18:04:48 +0800

    后台接口测试

--- src/main/java/net/qiujuer/web/italker/push/Application.java
+++ src/main/java/net/qiujuer/web/italker/push/Application.java
@@ -3,6 +3,7 @@ package net.qiujuer.web.italker.push;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import java.util.logging.Logger;
 
+import net.qiujuer.web.italker.push.provider.GsonProvider;
 import net.qiujuer.web.italker.push.service.AccountService;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -13,7 +14,8 @@ public class Application extends ResourceConfig{
         packages(AccountService.class.getPackage().getName());
 
         //注册Json解析器
-        register(JacksonJsonProvider.class);
+//        register(JacksonJsonProvider.class);
+        register(GsonProvider.class);
         //注册日志打印输出
         register(Logger.class);
     }
--- src/main/java/net/qiujuer/web/italker/push/service/AccountService.java
+++ src/main/java/net/qiujuer/web/italker/push/service/AccountService.java
@@ -1,5 +1,7 @@
 package net.qiujuer.web.italker.push.service;
 
+import net.qiujuer.web.italker.push.bean.api.account.RegisterModel;
+import net.qiujuer.web.italker.push.bean.card.UserCard;
 import net.qiujuer.web.italker.push.bean.db.User;
 
 import javax.ws.rs.*;
@@ -7,21 +9,15 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/account")
 public class AccountService {
-    //GET 127.0.0.1/api/account/login
-    @GET
-    @Path("/login")
-    public String get() {
-        return "You get the login.";
-    }
     @POST
-    @Path("/login")
+    @Path("/register")
     //指定请求与返回的响应体为JSON
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public User post(){
-        User user = new User();
-        user.setName("美女");
-        user.setSex(2);
-        return user;
+    public UserCard register(RegisterModel model){
+        UserCard card = new UserCard();
+        card.setName(model.getName());
+        card.setFollow(true);
+        return card;
     }
 }
